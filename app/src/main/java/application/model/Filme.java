15f015d0464df904e10f8d885337beb54d3b0dac
otaviobonacina <bonacina_otavio@hotmail.com>
2024-03-05 19:44:25 +0000

    get,put,del

--- app/src/main/java/application/model/Filme.java
+++ app/src/main/java/application/model/Filme.java
@@ -1,8 +1,37 @@
 package application.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name="filmes")
 public class Filme{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String sinopse;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getSinopse() {
+        return sinopse;
+    }
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
 
 }
---
+++ app/src/main/java/application/repository/FilmeRepository.java
@@ -0,0 +1,7 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import application.model.Filme;
+public interface FilmeRepository extends CrudRepository<Filme, Long> {
+    
+}
