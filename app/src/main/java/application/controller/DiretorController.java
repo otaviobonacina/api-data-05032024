ed518f4ab0b5b3e29df34f1d316e517bd9eb0e49
otaviobonacina <bonacina_otavio@hotmail.com>
2024-03-12 19:04:49 +0000

    e1

--- app/src/main/java/application/controller/DiretorController.java
+++ app/src/main/java/application/controller/DiretorController.java
@@ -3,9 +3,11 @@ package application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +30,20 @@ public class DiretorController {
     }
   
     @GetMapping("/diretores/{id}")
-    public Diretor getDiretor(PathVariable Long id ){
+    public Diretor getDiretor(@PathVariable Long id ){
         return diretorRepo.findById(id).get();
+    }
+    @PutMapping("/diretores/{id}")
+    public Diretor putDiretor(
+        @RequestBody Diretor diretor,
+        @PathVariable Long id){
+        Diretor resposta = diretorRepo.findById(id).get();
+        resposta.setNome( diretor.getNome());
+        return diretorRepo.save(resposta);
+    }
+    @DeleteMapping("/diretores/{id}")
+    public void deleteDiretores(@PathVariable long id){
+        diretorRepo.deleteById(id);
     }
 }
 
