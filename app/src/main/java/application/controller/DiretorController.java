e41ce845cc40a4109fb407412a086ca92f582782
otaviobonacina <bonacina_otavio@hotmail.com>
2024-03-12 17:28:24 +0000

    er

--- app/src/main/java/application/controller/DiretorController.java
+++ app/src/main/java/application/controller/DiretorController.java
@@ -1,12 +1,17 @@
 package application.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import application.model.Diretor;
 import application.repository.DiretorRepository;
+import javafx.scene.shape.Path;
 
 @RestController
 public class DiretorController {
@@ -17,4 +22,14 @@ public class DiretorController {
     public Diretor post(@RequestBody Diretor diretor) {
         return diretorRepo.save(diretor);
     }
+    @GetMapping("/diretores")
+    public List<Diretor> diretores(){
+        return(List<Diretor>) diretorRepo.findAll();
+    }
+  
+    @GetMapping("/diretores/{id}")
+    public Diretor getDiretor(PathVariable Long id ){
+        return diretorRepo.findById(id).get();
+    }
 }
+
